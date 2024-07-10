8bd6b465ae8f66b158d91c0e7d9d7e081a97fb90
nanimono <nan1mono@163.com>
2024-07-11 01:05:44 +0800

    feat：添加主页启动的额外信息

--- src/main/java/com/project/template/ApiApplication.java
+++ src/main/java/com/project/template/ApiApplication.java
@@ -1,13 +1,17 @@
 package com.project.template;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+        log.info("swagger json url:{}", "http://localhost:8250/v3/api-docs");
+        log.info("swagger doc.html url:{}", "http://localhost:8250/doc.html");
     }
 
 }
